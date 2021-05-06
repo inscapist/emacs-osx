1a04fc6362285bb139d4d0cbc5eb9fa8d058c6d3
Felix <zen9.felix@gmail.com>
2021-05-06 13:33:18 +0800

    Add sha256 for fetchTarball for passing pure evaluation mode

--- pkgs.nix
+++ pkgs.nix
@@ -1,8 +1,8 @@
 let
   # refer https://status.nixos.org for latest nixpkgs
   commit = "bb149eb1192f278f95d0b870140750aac1bd81c0";
-  pkgs = import
-    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz") {
-      overlays = [ (import ./emacs-overlay.nix) ];
-    };
+  pkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
+    sha256 = "0986q8li490qkig65mc86ppgr15q254hx5ij4z04m8iknkqc61k9";
+  }) { overlays = [ (import ./emacs-overlay.nix) ]; };
 in pkgs
