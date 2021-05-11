d7befc6676ef59f2a55a1e6a3c80fe10df90f75d
Felix <zen9.felix@gmail.com>
2021-05-11 13:57:06 +0800

    Remove unused files

--- .doom.d/README.md
+++
@@ -1,3 +0,0 @@
-# Build doom emacs with native compilation
-
-This is a failed attempt. I will try again when I got the time
--- .doom.d/packages.el
+++
@@ -1,7 +0,0 @@
-;; Extra packages
-(package! origami)
-(package! lsp-origami)
-(package! zoom)
-
-;; Themes
-(package! kaolin-themes)
