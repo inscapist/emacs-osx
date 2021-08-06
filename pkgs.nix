e57c13a2b0170a1bf5b17e0ff84b36c7fa9071c2
Felix <zen9.felix@gmail.com>
2021-08-06 10:57:22 +0800

    Update nixpkgs, in attempt to fix build

--- pkgs.nix
+++ pkgs.nix
@@ -1,8 +1,11 @@
 let
   # refer https://status.nixos.org for latest nixpkgs
-  commit = "5c7023e5f051e1f534c5bbac50f0d18320823b28";
+  commit = "b2d256095aeac09f8af6b6c95ab3cf4bc5fd4e6f";
   pkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
-    sha256 = "0v46qrjraamhxcf741z7k7fjpkgk2a3i3kjsa9q0jq18vcb2hwgn";
+
+    # nix-prefetch-url --unpack \
+    # https://github.com/NixOS/nixpkgs/archive/b2d256095aeac09f8af6b6c95ab3cf4bc5fd4e6f.tar.gz
+    sha256 = "06xamgm7w9xnn17lzg5kcmibch6a753rjc2xpf0z7jgw84rslff3";
   }) { overlays = [ (import ./emacs-overlay.nix) ]; };
 in pkgs
